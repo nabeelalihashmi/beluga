84798c1d5a2ae3bd37758996ef8fc930f8d7062f
Nabeel Ali <mail2nabeelali@gmail.com>
2022-09-13 11:27:03 +0500

    Done

--- belugalayya.php
+++ belugalayya.php
@@ -1,9 +1,9 @@
 <?php
 
-echo '<pre>';
 $h = fopen('file.txt', 'r');
-$res_a = [];
-$res_b = [];
+$f_bzu = [];
+$f_bzu2 = [];
+$datax = ['first' => [], 'second'=> []];
 while (($line = fgets($h)) !== false) {
     $line = trim($line);
     $data = explode(',', $line);
@@ -11,31 +11,24 @@ while (($line = fgets($h)) !== false) {
     $diff_b = array_diff(str_split($data[1]), str_split($data[0]));
     $pos_in_first = [];
     $pos_in_second = [];
-    $datax = [];
-    // echo '<h1> According to First </h1>';
     foreach($diff_a as $ltr) {
-        echo $line . "\r\n";
-       
         $pos = strpos($data[0], $ltr);
         $pos_in_first[] = $pos;
-        echo "[First]: ". $ltr . ' at position ' . $pos ."\r\n";
     }
-    $res_a[$ltr] = '[' . implode(',', $pos_in_first) . ']';
-    $datax[$line]['first'][] = $res_a[$ltr];
-    // echo '<h1> According to Second </h1>';
+    $datax['first'][$line] = $pos_in_first;
     foreach($diff_b as $ltr) {
-        echo $line . "\r\n";
         $pos = strpos($data[1], $ltr);
         $pos_in_second[] = $pos;
-        echo "[Second]: ". $ltr . ' at position' . $pos ."\r\n";
     }
-    $res_b[$ltr] = '[' . implode(',', $pos_in_second) . ']';
-    $datax[$line]['second'][] = $res_b[$ltr];
+    $datax['second'][$line] = $pos_in_second;
 }
 
-var_dump($datax);
-var_dump($res_a);
-var_dump($res_b);
-
-echo "Difference in first: " . implode(',', $res_a) . "\r\n";
-echo "Differences in second:" . implode(',', $res_b);
+echo '<h1> Diff According to First </h1><pre>';
+foreach($datax['first'] as $line => $pos) {
+    echo  $line . ',' . implode(',', $pos) . "\r\n";
+}
+echo '</pre><h1> Diff According to Second </h1><pre>';
+foreach($datax['second'] as $line => $pos) {
+    echo  $line . ',' . implode(',', $pos) . "\r\n";
+}
+echo '</pre>';
